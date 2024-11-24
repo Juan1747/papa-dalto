9cbf7c96d1a3bdd3844493217c056470a3e98727
JUAN DAVID CHACON MUÑOZ <139825492+Juan1747@users.noreply.github.com>
2024-11-24 14:35:43 -0500

    Add files via upload

---
+++ codigo.py
@@ -0,0 +1,5 @@
+nombre = "lucas"
+apellido = "dalto"
+
+saludo = "Hola " + nombre + " " + apellido + " Como estas?"
+print(saludo)
---
+++ readme.txt
@@ -0,0 +1,2 @@
+con la tecla 'a' te moves hacia la izquierda
+con la tecla 'd' te moves hacia la derecha
